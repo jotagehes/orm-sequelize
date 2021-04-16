d9657086ea6de9386fbdfcd1fe2b92d2f88a980a
jotagehes <joaogabriel-12@hotmail.com>
2021-04-16 15:25:30 -0300

    Criação das rotas delete e put

--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -6,5 +6,8 @@ const router = Router()
 router.get('/pessoas', PessoaController.pegaTodasAsPesoas)
 router.get('/pessoas/:id', PessoaController.pegarUmaPessoa)
 router.post('/pessoas', PessoaController.criaPessoa)
+router.put('/pessoas/:id', PessoaController.atualizaPessoa)
+router.delete('/pessoas/:id', PessoaController.apagaPessoa)
+
 
 module.exports = router
