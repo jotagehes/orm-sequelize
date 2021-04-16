05c904ea9ef53e027fbd50559a47f37c38e61138
jotagehes <joaogabriel-12@hotmail.com>
2021-04-16 15:10:20 -0300

    Criação da rota cria pessoas

--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -5,5 +5,6 @@ const router = Router()
 
 router.get('/pessoas', PessoaController.pegaTodasAsPesoas)
 router.get('/pessoas/:id', PessoaController.pegarUmaPessoa)
+router.post('/pessoas', PessoaController.criaPessoa)
 
 module.exports = router
