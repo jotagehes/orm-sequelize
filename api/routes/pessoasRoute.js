f3a277540400c5063f7487a3153997895dea94f5
jotagehes <joaogabriel-12@hotmail.com>
2021-04-16 14:56:22 -0300

    Criação da rota buscar pessoa por ID

--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -4,5 +4,6 @@ const PessoaController = require('../controllers/PessoaController')
 const router = Router()
 
 router.get('/pessoas', PessoaController.pegaTodasAsPesoas)
+router.get('/pessoas/:id', PessoaController.pegarUmaPessoa)
 
 module.exports = router
