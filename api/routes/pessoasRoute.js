9f79715ec96b0df5d5ae30d12d8ddfdb363200e9
jotagehes <62728338+jotagehes@users.noreply.github.com>
2021-04-19 09:55:11 -0300

    Adição do controle de matriculas dentro de pessoas

--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -8,6 +8,7 @@ router.get('/pessoas/:id', PessoaController.pegarUmaPessoa)
 router.post('/pessoas', PessoaController.criaPessoa)
 router.put('/pessoas/:id', PessoaController.atualizaPessoa)
 router.delete('/pessoas/:id', PessoaController.apagaPessoa)
+router.get('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.pegaUmaMatricula)
 
 
 module.exports = router
